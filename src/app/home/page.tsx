33cb6c0ec8821d8622ab6c92c1ded367ba0b0401
jmj <wjsdncl2222@gmail.com>
2024-06-19 14:51:42 +0900

    feat: home page 수정 및 footer 레이아웃 수정 - home에 추가할 내용이 무엇인지 임시로 작성 - home에 연락처 추가 - footer에 링크 추가 및 레이아웃 수정

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,17 @@
 export default function HomePage() {
 	return (
 		<main className='min-h-screen bg-_white dark:bg-_black'>
-			<article className='mx-auto my-0 min-h-screen bg-_white dark:bg-_black mobile:w-mobile tablet:w-tablet desktop:w-desktop'>
-				<span className=''>홈페이지입니다</span>
-			</article>
+			<div className='mx-auto my-0 flex min-h-screen flex-col justify-between bg-_white py-10 dark:bg-_black mobile:w-mobile tablet:w-tablet desktop:w-desktop'>
+				<section className='flex flex-col gap-7 text-center'>
+					<span className='text-3xl'>프론트엔드 개발자 블로그</span>
+					<span className='text-xl'>여기에는 블로그 메인글, 인기글이 들어갈 예정입니다</span>
+				</section>
+				<section className='relative bottom-0 flex flex-col text-right'>
+					<span>연락처</span>
+					<span>Email: wjsdncl2222@gmail.com</span>
+					<span>wlke12@naver.com</span>
+				</section>
+			</div>
 		</main>
 	);
 }
