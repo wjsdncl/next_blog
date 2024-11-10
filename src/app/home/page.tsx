bb9c3c2ded993e9ca594194807a7f2bc39eb9240
정민재 <89517903+wjsdncl@users.noreply.github.com>
2024-11-10 17:11:12 +0900

    Refactor : 사이트 메인, 블로그 메인 페이지 스타일 수정 (#21)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import ClientPage from "./_components/ClientPage";
 
 export default function Page() {
   return (
-    <div className="mx-auto flex size-full flex-col justify-between py-10 tablet:w-tablet desktop:w-desktop">
+    <div className="mx-auto flex size-full flex-col justify-between py-4 tablet:w-tablet desktop:w-desktop">
       <ClientPage />
     </div>
   );
