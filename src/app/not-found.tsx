e10b01809f59611b315d40d86ecb9fa7a8088d77
wjsdncl <wjsdncl2222@gmail.com>
2025-03-13 02:36:43 +0900

    style: 404페이지 디자인 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { NotFound } from "@/Icons/NotFound";
+
 export default function Page() {
   return (
-    <article className="mx-auto flex items-center justify-center py-40 tablet:w-tablet">
-      <span className="text-3xl">해당 페이지를 찾을 수 없습니다!</span>
+    <article className="mx-auto flex size-full grow flex-col items-center justify-center tablet:w-tablet">
+      <NotFound height={"auto"} width={"50%"} />
+      <span className="mt-6 text-xl font-semibold tablet:text-3xl">해당 페이지를 찾을 수 없습니다!</span>
+
+      <Link
+        href="/"
+        className="ml-4 mt-6 rounded-xl bg-gray-200 px-4 py-2 text-lg font-semibold tablet:mt-16 tablet:px-8 tablet:py-4 tablet:text-xl"
+      >
+        홈으로 돌아가기
+      </Link>
     </article>
   );
 }
