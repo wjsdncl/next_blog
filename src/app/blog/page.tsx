8314de5bd0829185af9ad13b1e174d8b406aeeb0
wjsdncl <wjsdncl2222@gmail.com>
2025-03-13 02:39:25 +0900

    fix: 빌드 오류 수정 - useSearchParams 사용 시 suspense를 사용하지 않아 발생한 오류 수정

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,13 @@
 import { type Metadata } from "next";
+import { Suspense } from "react";
 import ClientPage from "./_components/ClientPage";
 
 export default function Page() {
   return (
     <div className="relative mx-auto flex size-full flex-col justify-between px-5 py-8 tablet:w-tablet tablet:px-0">
-      <ClientPage />
+      <Suspense fallback={null}>
+        <ClientPage />
+      </Suspense>
     </div>
   );
 }
