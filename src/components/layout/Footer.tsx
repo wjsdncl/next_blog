0ecd506be695c7b938bc4b934073362fd71fe05d
wjsdncl <wjsdncl2222@gmail.com>
2026-04-04 21:33:37 +0900

    chore: 하단 날짜 수정 2024 > 2026

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <footer className="flex h-[56px] items-center justify-center bg-gray-200 p-4 text-center">
       <article className="flex items-center justify-between gap-4 tablet:w-tablet desktop:w-desktop">
-        <span className="text-nowrap font-medium">©wjdalswo 2024.</span>
+        <span className="text-nowrap font-medium">©wjdalswo 2026.</span>
         <div className="flex items-center justify-center gap-4 font-semibold">
           <Link href="https://github.com/wjsdncl" className="transition-colors duration-200 hover:text-brand-tertiary">
             Github
