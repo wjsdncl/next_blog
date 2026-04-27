aefbb43a9926d2dc5d050446c1a1399f6b9220cd
wjsdncl <wjsdncl2222@gmail.com>
2026-04-27 20:28:01 +0900

    chore: portfolio.api 함수 시그니처 포맷 정리

--- src/services/portfolio.api.ts
+++ src/services/portfolio.api.ts
@@ -123,7 +123,13 @@ export const createPortfolio = async (portfolioData: PortfolioRequest) => {
   }
 };
 
-export const updatePortfolio = async ({ id, portfolioData }: { id: string; portfolioData: Partial<PortfolioRequest> }) => {
+export const updatePortfolio = async ({
+  id,
+  portfolioData,
+}: {
+  id: string;
+  portfolioData: Partial<PortfolioRequest>;
+}) => {
   try {
     return await instance.PATCH<{ success: boolean; data: Portfolio }>(`/portfolios/${id}`, portfolioData);
   } catch (error) {
